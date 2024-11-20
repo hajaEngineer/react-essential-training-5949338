c816d746828f634c0979193adbb4b6d344a78d37
Haja <haja.randrianarisoa@outlook.com>
2024-11-20 16:20:55 +0000

    Progress until 03_02b

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,44 +1,200 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+/* eslint-disable react/prop-types */
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+/**02_02b*/
+// function App() {
 
+//   return (
+//    <h1>Hello React!</h1>
+//   );
+// }
+
+/**02_03b*/
+// let language = "React"
+// let moon = "Moon"
+
+// function App() {
+
+//   return (
+//    <h1>Hello {language.toUpperCase()} {moon}!</h1>
+//   );
+// }
+
+/**02_04b - Component*/
+// function Header() {
+//   return (
+//     <header>
+//       <h1>Eve&apos;s Kitchen</h1>
+//     </header>
+//   )
+// }
+
+// function App() {
+//   return (<div>
+//    <Header />
+//    <main>
+//     <h2>We serve the most delicious food around</h2>
+//    </main>
+//    </div>
+//   );
+// }
+
+/**02_05b*/
+// function Header(props) {
+//   return (
+//     <header>
+//       <h1>{props.name}'s Kitchen</h1>
+//       <p>Copyright {props.year}</p>
+//     </header>
+//   )
+// }
+
+// function App() {
+//   return (<div>
+//    <Header name="Alex" year={new Date().getFullYear()}/>
+//    <main>
+//     <h2>We serve the most delicious food around</h2>
+//    </main>
+//    </div>
+//   );
+// }
+
+/**02_06b*/
+// function Header({name, year}) {
+//   return (
+//     <header>
+//       <h1>{name}&rsquo;s Kitchen</h1>
+//       <p>Copyright {year}</p>
+//     </header>
+//   )
+// }
+
+// function App() {
+//   return (<div>
+//    <Header name="Alex" year={new Date().getFullYear()}/>
+//    <main>
+//     <h2>We serve the most delicious food around</h2>
+//    </main>
+//    </div>
+//   );
+// }
+
+/**03_01b*/
+// function Header({name, year}) {
+//   return (
+//   <header>
+//     <h1>{name}&apos;s Kitchen</h1>
+//   <p>Copyright {year}</p>
+//   </header>
+//   );
+// }
+// const items = [
+//   "Macaroni and Cheese",
+//   "Salmon with Potatoes",
+//   "Tofu with Vegetables"
+// ]
+
+// function Main({ dishes }) {
+//   return (
+//     <ul>
+//       {dishes.map((dish, index) => (
+//         <li key={index} style={{ listStyleType: "none" }}>
+//           {dish}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// function App() {
+//   return (<div>
+//    <Header name="Alex" year={new Date().getFullYear()}/>
+//    <Main dishes={items}/>
+//    </div>
+//   );
+// }
+
+/**03_02b*/
+// function Header({name, year}) {
+//   return (
+//   <header>
+//     <h1>{name}&apos;s Kitchen</h1>
+//   <p>Copyright {year}</p>
+//   </header>
+//   );
+// }
+// const items = [
+//   "Macaroni and Cheese",
+//   "Salmon with Potatoes",
+//   "Tofu with Vegetables",
+//   "Minestrone Soup"
+// ]
+
+// const dishObjects = items.map((dish, i)=>({
+//   id: i,
+//   title: dish
+// }))
+// console.log(dishObjects);
+
+// function Main({ dishes }) {
+//   return (
+//     <ul>
+//       {dishes.map((dish) => (
+//         <li key={dish.id} style={{ listStyleType: "none" }}>
+//           {dish.title}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// function App() {
+//   return (<div>
+//    <Header name="Alex" year={new Date().getFullYear()}/>
+//    <Main dishes={dishObjects}/>
+//    </div>
+//   );
+// }
+
+/**03_03b*/
+function Header({name, year}) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>My React Project!</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <header>
+    <h1>{name}&apos;s Kitchen</h1>
+  <p>Copyright {year}</p>
+  </header>
+  );
+}
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup"
+]
+
+const dishObjects = items.map((dish, i)=>({
+  id: i,
+  title: dish
+}))
+console.log(dishObjects);
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li key={dish.id} style={{ listStyleType: "none" }}>
+          {dish.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  return (<div>
+   <Header name="Alex" year={new Date().getFullYear()}/>
+   <Main dishes={dishObjects}/>
+   </div>
   );
 }
 
